a4f64ebe0a50ec78b4e5ed02cf748d3cb7ce2d76
dfbb <fanghan@gmail.com>
2022-02-27 04:49:24 +0800

    Release

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ local WclPlayerScore = _G.LibStub("AceAddon-3.0"):NewAddon("WclPlayerScore", "Ac
 
 SLASH_WP_Commands1 = "/wcl"
 SlashCmdList["WP_Commands"] = function(msg)
-	print "WCLPlayerScore Version: 1.8.526 Date:20220226 "
+	print "WCLPlayerScore Version: 1.8.527 Date:20220227 "
 end
 
 local function expand(name)
